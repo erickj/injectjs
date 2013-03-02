67d31f14509d6ecbcbd27819dbbf962763d92aa3
Erick Johnson <erick@ejjohnson.org>
2013-03-02 17:22:23 -0500

    added fail expectation

--- spec/injectjs_spec.js
+++ spec/injectjs_spec.js
@@ -18,32 +18,4 @@ describe('injectjs', function() {
       fail();
     });
   });
-
-  describe('<static>.extend', function() {
-    var xVal = "x";
-    var Parent = (
-      function() {
-        var Ctor = function() {
-          this.x = xVal;
-        };
-
-        Ctor.prototype.getX = function() {
-          return this.x;
-        };
-
-        return Ctor;
-      }());
-
-    it('should setup a prototype chain', function() {
-      var Child = function() {
-        this.y = "y";
-      }
-      injectjs.extend(Child, Parent);
-
-      var c = new Child();
-      expext(c.getX()).toBe(xVal);
-      expect(c.y).toBe("y");
-      expect(c.constructor).toBe(Child);
-    });
-  });
 });
--- spec/spechelper.js
+++ spec/spechelper.js
@@ -1,7 +1,14 @@
 goog.provide('spechelper');
 
+var fail = function() {
+  expect('it').toFail();
+};
+
 beforeEach(function() {
   this.addMatchers({
+    toFail: function() {
+      return false;
+    },
     toBeA: function(type) {
       return this.actual instanceof type;
     }
