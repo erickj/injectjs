90539b64485866177e1ecfe9922f6c65c18e2ebe
Erick Johnson <erick@ejjohnson.org>
2013-03-03 00:17:09 -0500

    spec/spechelper: added toThrowInstanceOf matcher

--- spec/spechelper.js
+++ spec/spechelper.js
@@ -11,6 +11,20 @@ beforeEach(function() {
     },
     toBeA: function(type) {
       return this.actual instanceof type;
+    },
+    toThrowInstanceOf: function(type) {
+      var result = false;
+      var actual = this.actual;
+      if (typeof actual != 'function') {
+        throw new Error('Actual is not a function');
+      }
+      try {
+        actual();
+      } catch(e) {
+        result = e instanceof type;
+      }
+
+      return result;
     }
   });
 });
